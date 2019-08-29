621ef39a547df0c2bb2943ff44e4ddee173df72c
Mayuri Rathod <mayuri.rathod@imaginea.com>
2019-08-29 19:24:02 +0530

    Added components in routing

--- packages/angular-client/src/app/app-routing.module.ts
+++ packages/angular-client/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListNoteComponent } from './list-note/list-note.component';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { EditNoteComponent } from './edit-note/edit-note.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "list",
+    component: ListNoteComponent
+  }, {
+    path: "add",
+    component: AddNoteComponent
+  }, {
+    path: "edit/:id",
+    component: EditNoteComponent
+  }, {
+    path: "**",
+    pathMatch: "full",
+    redirectTo: "/list"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
