cc9775ed8d1d0a2a742d4c2c631a32cf2dbb9a2d
Mayuri Rathod <mayuri.rathod@imaginea.com>
2019-08-29 19:24:12 +0530

    DEclared components

--- packages/angular-client/src/app/app.module.ts
+++ packages/angular-client/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListNoteComponent } from './list-note/list-note.component';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { EditNoteComponent } from './edit-note/edit-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListNoteComponent,
+    AddNoteComponent,
+    EditNoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
